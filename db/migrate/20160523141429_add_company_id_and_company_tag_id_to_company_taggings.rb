915b58a40acbe999207527c6b8291d74fd8e62ec
Peter <ossipov.pietro@gmail.com>
2016-05-31 09:33:18 +0300

    Added puma to production. Deployed to Heroku. Issue fix 1

--- db/migrate/20160523141429_add_company_id_and_company_tag_id_to_company_taggings.rb
+++
@@ -1,6 +0,0 @@
-class AddCompanyIdAndCompanyTagIdToCompanyTaggings < ActiveRecord::Migration
-  def change
-    add_column :company_taggings, :company_id, :integer
-    add_column :company_taggings, :company_tag_id, :integer
-  end
-end
